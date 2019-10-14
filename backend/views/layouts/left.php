15d0ddc9c40db7fd7d14b91e15e2ed6bbdad434e
DmitriyDatskovskiy <dadzki@yandex.ru>
2019-10-14 16:29:20 +0400

    order

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -32,6 +32,8 @@
                 'items' => [
                     ['label' => 'Management', 'options' => ['class' => 'header']],
                     ['label' => 'Shop', 'icon' => 'folder', 'items' => [
+                        ['label' => 'Orders', 'icon' => 'file-o', 'url' => ['/shop/order/index'], 'active' => $this->context->id == 'shop/order'],
+
                         ['label' => 'Products', 'icon' => 'file-o', 'url' => ['/shop/product/index'], 'active' => $this->context->id == 'shop/product'],
 
                         ['label' => 'Brands', 'icon' => 'file-o', 'url' => ['/shop/brand/index'], 'active' => $this->context->id == 'shop/brand'],
