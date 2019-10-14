ed4042d0ea1e1125138c8cae78752a65b42f28dc
DmitriyDatskovskiy <dadzki@yandex.ru>
2019-10-14 13:30:57 +0400

    delivery, order

---
+++ console/migrations/m191014_090028_create_shop_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%shop_orders}}`.
+ */
+class m191014_090028_create_shop_orders_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%shop_orders}}', [
+            'id' => $this->primaryKey(),
+            'created_at' => $this->integer()->notNull(),
+            'user_id' => $this->integer()->notNull(),
+            'delivery_method_id' => $this->integer(),
+            'delivery_method_name' => $this->string()->notNull(),
+            'delivery_cost' => $this->integer()->notNull(),
+            'payment_method' => $this->string(),
+            'cost' => $this->integer()->notNull(),
+            'note' => $this->text(),
+            'current_status' => $this->integer()->notNull(),
+            'cancel_reason' => $this->text(),
+            'statuses_json' => 'JSON NOT NULL',
+            'customer_phone' => $this->string(),
+            'customer_name' => $this->string(),
+            'delivery_index' => $this->string(),
+            'delivery_address' => $this->text(),
+        ]);
+
+        $this->createIndex('{{%idx-shop_orders-user_id}}', '{{%shop_orders}}', 'user_id');
+        $this->createIndex('{{%idx-shop_orders-delivery_method_id}}', '{{%shop_orders}}', 'delivery_method_id');
+
+        $this->addForeignKey('{{%fk-shop_orders-user_id}}', '{{%shop_orders}}', 'user_id', '{{%user}}', 'id', 'CASCADE');
+        $this->addForeignKey('{{%fk-shop_orders-delivery_method_id}}', '{{%shop_orders}}', 'delivery_method_id', '{{%shop_delivery_methods}}', 'id', 'SET NULL');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%shop_orders}}');
+    }
+}
---
+++ shop/entities/Shop/queries/DeliveryMethodQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace shop\entities\Shop\queries;
+
+
+use yii\db\ActiveQuery;
+
+class DeliveryMethodQuery extends ActiveQuery
+{
+    public function availableForWeight($weight)
+    {
+        return $this->andWhere(['and',
+            ['or', ['min_weight' => null], ['<=', 'min_weight', $weight]],
+            ['or', ['max_weight' => null], ['>=', 'max_weight', $weight]],
+        ]);
+    }
+}
